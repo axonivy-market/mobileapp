96a14eb71a84ef92601c8c15b4388d893cc953c2
muoi.vovan <muoi.vovan@axonactive.com>
2023-11-26 14:35:13 +0700

    init base code

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,3 +1,4 @@
+import 'package:axon_ivy/page/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,43 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Axon Ivy Dev'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Dev App',
-            ),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      home: const HomeScreen(),
     );
   }
 }
