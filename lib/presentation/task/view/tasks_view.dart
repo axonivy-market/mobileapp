3bc364ff9112f1318ecc7dbf385c14cf7ba3331f
lmtung-axonivy <tung.leminh@axonactive.com>
2023-11-29 09:17:57 +0700

    update

--- lib/presentation/task/view/tasks_view.dart
+++ lib/presentation/task/view/tasks_view.dart
@@ -1,8 +1,56 @@
 import 'package:axon_ivy/core/generated/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class TasksView extends StatelessWidget {
+class TasksView extends StatefulWidget {
   const TasksView({super.key});
+
+  @override
+  State<TasksView> createState() => _TasksViewState();
+}
+
+class _TasksViewState extends State<TasksView> {
+  late final WebViewController _controller;
+  var loadingPercentage = 0;
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..loadRequest(
+        Uri.parse('http://10.0.2.2:8081/designer/pro/test-task\$1/18C1540E90A35E65/18C1540E90A35E65-f3/TaskA.ivp?taskId=14'),
+      )
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+        onNavigationRequest: (navigation) {
+          final host = Uri.parse(navigation.url).host;
+          if (host.contains('youtube.com')) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'Blocking navigation to $host',
+                ),
+              ),
+            );
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +60,7 @@ class TasksView extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Row(
+              mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 AppAssets.icons.logo.svg(),
@@ -24,7 +73,11 @@ class TasksView extends StatelessWidget {
                 )
               ],
             ),
-          )
+          ),
+          Expanded(
+              child: WebViewWidget(
+            controller: _controller,
+          ))
         ],
       ),
     );
