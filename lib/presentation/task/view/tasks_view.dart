8eb1e40744c2d0eaf1807ec9fa7a697a5e5fcac7
muoi.vovan <muoi.vovan@axonactive.com>
2023-12-08 10:09:59 +0700

    MIVY-923-956-set-up-api-service

--- lib/presentation/task/view/tasks_view.dart
+++ lib/presentation/task/view/tasks_view.dart
@@ -1,10 +1,16 @@
+<<<<<<< HEAD
 import 'dart:math';
 
 import 'package:axon_ivy/core/generated/colors.gen.dart';
 import 'package:axon_ivy/presentation/task/view/widgets/task_empty_widget.dart';
+=======
+import 'package:axon_ivy/presentation/task/bloc/task_bloc.dart';
+>>>>>>> e7517ab (MIVY-923-956-set-up-api-service)
 import 'package:axon_ivy/presentation/task/view/widgets/task_item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 import '../../../util/widgets/filter_widget.dart';
 import '../../../util/widgets/home_appbar.dart';
@@ -31,10 +37,31 @@ class _TasksViewState extends State<TasksView> {
     setState(() {
       items = List.generate(10, (index) => 'Refreshed Task $index');
     });
+=======
+import '../../../core/di/di_setup.dart';
+import '../../base_view/base_view.dart';
+import '../../util/widgets/bot_toast_helper.dart';
+
+class TasksView extends BasePageScreen {
+  const TasksView({super.key});
+
+  @override
+  State<TasksView> createState() => _TasksViewState();
+}
+
+class _TasksViewState extends BasePageScreenState<TasksView> {
+  late final TaskBloc _taskBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _taskBloc = getIt<TaskBloc>();
+>>>>>>> e7517ab (MIVY-923-956-set-up-api-service)
   }
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     return Scaffold(
       appBar: const HomeAppBar(),
       body: RefreshIndicator(
@@ -69,6 +96,50 @@ class _TasksViewState extends State<TasksView> {
                   },
                 )
               : const TaskEmptyWidget(),
+=======
+    return BlocProvider(
+      create: (context) => _taskBloc..add(const TaskEvent.getTasks()),
+      child: BlocListener<TaskBloc, TaskState>(
+        listener: (context, state) {
+          if (state is TaskLoadingState) {
+            if (state.isShowLoading) {
+              showAppLoading();
+            } else {
+              hideLoading();
+            }
+          }
+        },
+        child: Scaffold(
+          appBar: const HomeAppBar(),
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: ListView.builder(
+              padding: const EdgeInsets.only(top: 20),
+              itemCount: 1,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: [
+                    const FilterWidget(),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          children: [
+                            TaskItemWidget(),
+                            const SizedBox(height: 10),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+>>>>>>> e7517ab (MIVY-923-956-set-up-api-service)
         ),
       ),
     );
