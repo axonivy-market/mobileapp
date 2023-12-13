73187276336fc32c03c2f4a7e3fe679ca672ea2a
lmtung-axonivy <tung.leminh@axonactive.com>
2023-12-13 17:34:29 +0700

    MIVY-960 Mapping response data to UI > Update appbar UI

--- lib/util/widgets/home_appbar.dart
+++ lib/util/widgets/home_appbar.dart
@@ -3,11 +3,13 @@ import 'package:axon_ivy/core/generated/colors.gen.dart';
 import 'package:flutter/material.dart';
 
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const HomeAppBar({
-    super.key,
-  });
+  const HomeAppBar({super.key, this.isShowLastUpdated = false});
+
+  final bool isShowLastUpdated;
+
   @override
   Size get preferredSize => const Size.fromHeight(50);
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -18,14 +20,16 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
         child: AppAssets.icons.logo.svg(),
       ),
       actions: [
-        const Text(
-          'last updated 20:17',
-          style: TextStyle(color: AppColors.babyTalkGrey),
-        ),
+        isShowLastUpdated
+            ? const Text(
+                'last updated 20:17',
+                style: TextStyle(color: AppColors.babyTalkGrey),
+              )
+            : Container(),
         const SizedBox(
           width: 10,
         ),
-        AppAssets.icons.offline.svg(),
+        isShowLastUpdated ? AppAssets.icons.offline.svg() : Container(),
         IconButton(
           onPressed: () {},
           icon: AppAssets.icons.notification.svg(),
