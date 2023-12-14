95da1f19aeaac55af1386b6357307bb18dd94907
lmtung-axonivy <tung.leminh@axonactive.com>
2023-12-14 11:34:58 +0700

    Fix missing gen file

---
+++ lib/data/models/processes/process.freezed.dart
@@ -0,0 +1,240 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'process.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Process _$ProcessFromJson(Map<String, dynamic> json) {
+  return _Process.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Process {
+  int get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  String get activatorName => throw _privateConstructorUsedError;
+  String get fullRequestPath => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ProcessCopyWith<Process> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProcessCopyWith<$Res> {
+  factory $ProcessCopyWith(Process value, $Res Function(Process) then) =
+      _$ProcessCopyWithImpl<$Res, Process>;
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      String description,
+      String activatorName,
+      String fullRequestPath});
+}
+
+/// @nodoc
+class _$ProcessCopyWithImpl<$Res, $Val extends Process>
+    implements $ProcessCopyWith<$Res> {
+  _$ProcessCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? description = null,
+    Object? activatorName = null,
+    Object? fullRequestPath = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      activatorName: null == activatorName
+          ? _value.activatorName
+          : activatorName // ignore: cast_nullable_to_non_nullable
+              as String,
+      fullRequestPath: null == fullRequestPath
+          ? _value.fullRequestPath
+          : fullRequestPath // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ProcessImplCopyWith<$Res> implements $ProcessCopyWith<$Res> {
+  factory _$$ProcessImplCopyWith(
+          _$ProcessImpl value, $Res Function(_$ProcessImpl) then) =
+      __$$ProcessImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      String description,
+      String activatorName,
+      String fullRequestPath});
+}
+
+/// @nodoc
+class __$$ProcessImplCopyWithImpl<$Res>
+    extends _$ProcessCopyWithImpl<$Res, _$ProcessImpl>
+    implements _$$ProcessImplCopyWith<$Res> {
+  __$$ProcessImplCopyWithImpl(
+      _$ProcessImpl _value, $Res Function(_$ProcessImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? description = null,
+    Object? activatorName = null,
+    Object? fullRequestPath = null,
+  }) {
+    return _then(_$ProcessImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      activatorName: null == activatorName
+          ? _value.activatorName
+          : activatorName // ignore: cast_nullable_to_non_nullable
+              as String,
+      fullRequestPath: null == fullRequestPath
+          ? _value.fullRequestPath
+          : fullRequestPath // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ProcessImpl implements _Process {
+  const _$ProcessImpl(
+      {this.id = 0,
+      this.name = "",
+      this.description = "",
+      this.activatorName = "",
+      this.fullRequestPath = ""});
+
+  factory _$ProcessImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ProcessImplFromJson(json);
+
+  @override
+  @JsonKey()
+  final int id;
+  @override
+  @JsonKey()
+  final String name;
+  @override
+  @JsonKey()
+  final String description;
+  @override
+  @JsonKey()
+  final String activatorName;
+  @override
+  @JsonKey()
+  final String fullRequestPath;
+
+  @override
+  String toString() {
+    return 'Process(id: $id, name: $name, description: $description, activatorName: $activatorName, fullRequestPath: $fullRequestPath)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ProcessImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.activatorName, activatorName) ||
+                other.activatorName == activatorName) &&
+            (identical(other.fullRequestPath, fullRequestPath) ||
+                other.fullRequestPath == fullRequestPath));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, id, name, description, activatorName, fullRequestPath);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ProcessImplCopyWith<_$ProcessImpl> get copyWith =>
+      __$$ProcessImplCopyWithImpl<_$ProcessImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ProcessImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Process implements Process {
+  const factory _Process(
+      {final int id,
+      final String name,
+      final String description,
+      final String activatorName,
+      final String fullRequestPath}) = _$ProcessImpl;
+
+  factory _Process.fromJson(Map<String, dynamic> json) = _$ProcessImpl.fromJson;
+
+  @override
+  int get id;
+  @override
+  String get name;
+  @override
+  String get description;
+  @override
+  String get activatorName;
+  @override
+  String get fullRequestPath;
+  @override
+  @JsonKey(ignore: true)
+  _$$ProcessImplCopyWith<_$ProcessImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
