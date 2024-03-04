e7c10ad95c1f662b76e4df3731dfe9115143736b
lmtung-axonivy <tung.leminh@axonactive.com>
2024-03-04 10:02:30 +0700

    MIVY-1045 Display process icon from server

---
+++ lib/data/models/processes/customfield/customfield.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'customfield.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CustomFieldImpl _$$CustomFieldImplFromJson(Map<String, dynamic> json) =>
+    _$CustomFieldImpl(
+      name: json['name'] as String? ?? '',
+      value: json['value'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$$CustomFieldImplToJson(_$CustomFieldImpl instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'value': instance.value,
+    };
