9a3093340e29ec56502f9f2e73c5a5eea23e445f
vvmuoi-axonivy <muoi.vovan@axonactive.com>
2024-04-05 14:05:07 +0700

    Fix import package

--- lib/features/profile/presentation/bloc/logged_cubit/logged_in_cubit.dart
+++ lib/features/profile/presentation/bloc/logged_cubit/logged_in_cubit.dart
@@ -4,17 +4,16 @@ import 'package:axon_ivy/core/app/app_config.dart';
 import 'package:axon_ivy/core/app/app_constants.dart';
 import 'package:axon_ivy/core/app/demo_config.dart';
 import 'package:axon_ivy/core/di/di_setup.dart';
-import 'package:axon_ivy/core/extensions/string_ext.dart';
-import 'package:axon_ivy/core/utils/shared_preference.dart';
+import 'package:axon_ivy/shared/extensions/string_ext.dart';
+import 'package:axon_ivy/shared/storage/shared_preference.dart';
 import 'package:crypto/crypto.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:injectable/injectable.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'logged_in_state.dart';
+import 'package:injectable/injectable.dart';
 
 part 'logged_in_cubit.freezed.dart';
+part 'logged_in_state.dart';
 
 @injectable
 class LoggedInCubit extends Cubit<LoggedInState> {
@@ -41,11 +40,10 @@ class LoggedInCubit extends Cubit<LoggedInState> {
   }
 
   void setDemoUser() {
-
     SharedPrefs.setIsLogin(true);
-    
+
     getIt<Dio>().options.baseUrl = DemoConfig.demoServerUrl.isEmptyOrNull
-          ? AppConfig.baseUrl
+        ? AppConfig.baseUrl
         : DemoConfig.demoServerUrl;
   }
 }
