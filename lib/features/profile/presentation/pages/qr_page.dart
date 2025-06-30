ced9f3c2eba188ccc46b84a838b111b4f2277af3
nhthinh-axonivy <nhthinh@axonactive.com>
2025-07-01 05:57:43 +0700

    IVYPORTAL-19158 Fix issues with your Play Console developer account, Axon Ivy Mobile App

--- lib/features/profile/presentation/pages/qr_page.dart
+++ lib/features/profile/presentation/pages/qr_page.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'dart:io';
 
 import 'package:axon_ivy/features/profile/domain/entities/qr/qr_model.dart';
 import 'package:axon_ivy/shared/widgets/back_button_widget.dart';
@@ -9,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class QRParentPage extends StatefulWidget {
   const QRParentPage({super.key});
@@ -19,17 +18,13 @@ class QRParentPage extends StatefulWidget {
 }
 
 class _QRParentPageState extends State<QRParentPage> {
-  Barcode? result;
-  QRViewController? controller;
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  late BuildContext qrViewContext;
+  BarcodeCapture? result;
+  MobileScannerController controller = MobileScannerController();
+
   @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    }
-    controller!.resumeCamera();
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -56,54 +51,37 @@ class _QRParentPageState extends State<QRParentPage> {
             MediaQuery.of(context).size.height < 400.h)
         ? 150.0.h
         : 300.0.h;
-    return QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-      overlay: QrScannerOverlayShape(
-          borderColor: Colors.red,
-          borderRadius: 10.r,
-          borderLength: 30.r,
-          borderWidth: 10.r,
-          cutOutSize: scanArea),
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    return Center(
+      child: SizedBox(
+        width: scanArea,
+        height: scanArea,
+        child: MobileScanner(
+          controller: controller,
+          onDetect: (BarcodeCapture capture) {
+            if (result == null && capture.barcodes.isNotEmpty) {
+              setState(() {
+                result = capture;
+              });
+              final code = capture.barcodes.first.rawValue;
+              debugPrint(code);
+              if (code != null &&
+                  code.contains("loginUrl") &&
+                  code.contains("username")) {
+                try {
+                  Map<String, dynamic> data = jsonDecode(code);
+                  final model = QRModel.fromJson(data);
+                  context.pop({model});
+                } catch (e) {
+                  log('Failed to parse QR: $e');
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("qr.invalidData".tr())),
+                  );
+                }
+              }
+            }
+          },
+        ),
+      ),
     );
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-    controller.scannedDataStream.listen((scanData) {
-      if (result == null) {
-        setState(() {
-          result ??= scanData;
-        });
-        debugPrint("${result?.code}");
-        if (result?.code != null) {
-          if (result!.code!.contains("loginUrl") &&
-              result!.code!.contains("username")) {
-            String jsonData = result!.code!;
-            Map<String, dynamic> data = jsonDecode(jsonData);
-            final model = QRModel.fromJson(data);
-            context.pop({model});
-          }
-        }
-      }
-    });
-  }
-
-  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
-    if (!p) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("qr.noPermission".tr())),
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
   }
 }
