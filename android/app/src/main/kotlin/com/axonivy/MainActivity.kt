3f9defdfc646c6d8524886df2469734d588b8351
Tung Le <tung.le@axonactive.com>
2026-02-27 17:53:43 +0700

    MIVY-1287 Security vulnerabilities - udpated implementation

--- android/app/src/main/kotlin/com/axonivy/MainActivity.kt
+++ android/app/src/main/kotlin/com/axonivy/MainActivity.kt
@@ -6,7 +6,7 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
     }
 }
